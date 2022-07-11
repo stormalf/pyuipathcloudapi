a56ff37788ccee4dbae24e50782863357c6d33b9
stormalf <stormalf.summonerswar@gmail.com>
2022-07-11 19:01:13 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # pyuipathcloudapi
-python3 api to connect to uipath cloud orchestrator 
+
+python3 api to connect to uipath cloud orchestrator.
+
+Not sure to keep it as the other apis modules that I created.
+The main issue here is that we need to generate a token and you can use it for some time after you need to regenerate a new one.
+For now, I decided to add a parameter -g to generate the token and in this case the token is used to access to the API.
+
+The difficulty here is we need : one url to generate the token and one other to access to the APi using the token generated.
+
+## api url
+
+the api url is build with :
+
+    "https://cloud.uipath.com" + '/' + your_organization_unit + '/' + your_tenant_name + '/' + "orchestrator_" + endpoint_api
+
+    example :
+
+        https://cloud.uipath.com/myorg/mytenant/orchestrator_api/odata/Machines
+
+## environment variables
+
+UIPATH_UNIT=your_organization_unit
+UIPATH_TENANTNAME=your_tenant_name
+UIPATH_FILE=your_uipath_file
+
+The content of UIPATH_FILE :
+
+{
+"grant_type": "refresh_token",
+"client_id": "{your_client_id}",
+"refresh_token": "{your_user_key}"
+}
+
+## usage
+
+./pyuipathcloudapi.py --help
+
+    usage: pyuipathcloudapi.py [-h] [-V] [-U UNIT] [-T TENANTNAME] [-F UIPATHFILE] [-u URL] [-a API] [-m METHOD]
+                            [-J JSONFILE] [-g]
+
+    pyuipathcloudapi is a python3 program that call uipath cloud apis in command line or imported as a module
+
+    optional arguments:
+    -h, --help            show this help message and exit
+    -V, --version         Display the version of pyuipathcloudapi
+    -U UNIT, --unit UNIT  uipathcloud organization unit id
+    -T TENANTNAME, --tenantname TENANTNAME
+                            uipathcloud tenant name
+    -F UIPATHFILE, --uipathfile UIPATHFILE
+                            uipathcloud file needed to generate token
+    -u URL, --url URL     uipathcloud url
+    -a API, --api API     uipathcloud api should start by a slash
+    -m METHOD, --method METHOD
+                            should contain one of the method to use : ['DELETE', 'GET', 'POST', 'PUT']
+    -J JSONFILE, --jsonfile JSONFILE
+                            json file needed for POST method
+    -g, --generatetoken   generate token first before calling API
+
+## example
+
+    ./pyuipathcloudapi.py -a /odata/Machines -g
+
+## TODO
+
+- testing all the parameters
+- split into two modules one for generating the token and one for executing using the token generated previously ?
+- add a parameter to generate the token and store it in a file .env ? and using this token to call the api ?
---
+++ pyuipathcloudapi.py
@@ -0,0 +1,218 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from cryptography.fernet import Fernet
+import requests
+from json import loads as jsonload
+import argparse
+import os
+
+'''
+pyuipathcloudapi.py is to be used by other python modules to automate uipath cloud api usage.
+it could be called in command line.
+See uipath cloud official references to get the correct Api URL and method to use :
+
+    
+'''
+
+__version__ = "1.0.0"
+
+ALLOWED_METHODS = ["DELETE", "GET", "POST", "PUT"]
+URL = "https://cloud.uipath.com"
+NO_CONTENT = 204
+HEADER_JSON = "application/json"
+ORCHESTRATOR = "orchestrator_"
+
+def pyuipathcloudApiVersion():
+    return f"pyuipathcloudapi version : {__version__}"
+
+
+
+
+class uipathcloudApi():
+    def __init__(self, api, method, url,  token, jsonfile, unit, tenant):
+        self.api = api
+        self.method = method
+        self.json = jsonfile
+        self.url = url
+        if token == None:
+            self.token = None
+        else:
+            self.token = uipathcloudApi.crypted(token)
+        self.unit = unit
+        self.tenant = tenant
+
+
+    def __repr__(self):
+        return (f"uipathcloudApi api: {self.api}, method: {self.method}, url: {self.url}")
+
+    #return the encrypted password/token
+    @classmethod
+    def crypted(cls, token):
+        cls.privkey = Fernet.generate_key()        
+        cipher_suite = Fernet(cls.privkey)
+        ciphered_text = cipher_suite.encrypt(token.encode())
+        cls.token = ciphered_text
+        return cls.token
+
+    #return the decrypted password/token
+    @classmethod
+    def decrypted(cls, token):
+        cls.token = token
+        cipher_suite = Fernet(cls.privkey)
+        decrypted_text = cipher_suite.decrypt(cls.token)
+        decrypted_text = decrypted_text.decode()
+        return decrypted_text
+
+    #execute the uipathcloud api using a temp instance
+    @staticmethod
+    def runuipathcloudApi(api, method, url, token, json, unit, tenant):
+        tempuipathcloud = uipathcloudApi(api, method, url, token, json, unit, tenant)
+        response = tempuipathcloud.uipathcloudAuthentication()
+        tempuipathcloud = None
+        return response       
+
+
+    #egenerate a token
+    @staticmethod
+    def generatetoken(tenantname, json):
+        api = "/oauth/token"
+        url = "https://account.uipath.com"
+        response = "{}"
+        if json == None:
+            response = jsonload('{"message": "Error : uipath file  missing! define at least UIPATH_FILE environment variable"}')
+            return response 
+        if tenantname == None:
+            response = jsonload('{"message": "Error : tenant name missing!" define at least UIPATH_TENANTNAME environment variable"}')
+            return response             
+        try:
+            apiurl = url + api  
+            header = {}
+            header['Accept'] = HEADER_JSON
+            header['Content-Type'] = HEADER_JSON     
+            header['X-UIPATH-TenantName'] = tenantname 
+            contents = open(json, 'rb')
+            response = requests.post(apiurl, headers=header, data=contents)
+            #print(response)
+            contents.close()       
+        except requests.exceptions.RequestException as e:  
+            raise SystemExit(e)   
+        if response.status_code == NO_CONTENT:
+            response = "{}"
+        elif response.status_code != 200:
+            response = jsonload('{"message": "Error : ' + str(response.status_code) + ' ' + response.reason + '"}')
+        else:            
+            response = response.json()    
+            #print(response)                 
+        return response       
+
+
+    #call private function
+    def uipathcloudAuthentication(self):
+        response = self.__uipathcloudTokenAuth()
+        return response
+
+    #internal function that formats the url and calls the uipathcloud apis
+    def __uipathcloudTokenAuth(self):
+        apiurl = self.url + '/' + self.unit + '/' + self.tenant + '/' + ORCHESTRATOR + self.api  
+        header = {}
+        header['Accept'] = HEADER_JSON
+        # if self.unit != None:
+        #     header['X-UIPATH-OrganizationUnitId'] = self.unit  
+        if self.token != None:       
+            header['Authorization'] = "Bearer " + uipathcloudApi.decrypted(self.token)  
+        response = self.__uipathcloudDispatch(apiurl,  header)
+        return response
+
+    #internal function that calls the requests
+    def __uipathcloudDispatch(self, apiurl,  header):
+        response = "{}"        
+        try:
+            if self.method == "POST":
+                contents = open(self.json, 'rb')
+                header['Content-Type'] = HEADER_JSON
+                response = requests.post(apiurl, headers=header, data=contents)
+                contents.close()
+            elif self.method == "GET":
+                response = requests.get(apiurl,  headers=header)
+            elif self.method == "PUT":
+                if self.json == '':
+                    response = requests.put(apiurl,  headers=header)
+                else:
+                    contents = open(self.json, 'rb') 
+                    header['Content-Type'] = HEADER_JSON                   
+                    response = requests.put(apiurl,  headers=header, data=contents)
+                    contents.close()
+            elif self.method == "DELETE":
+                #raise Exception("DELETE not implemented yet")
+                response = requests.delete(apiurl,  headers=header)  
+        except requests.exceptions.RequestException as e:  
+            raise SystemExit(e)   
+        if response.status_code == NO_CONTENT:
+            response = "{}"
+        elif response.status_code != 200:
+            response = jsonload('{"message": "Error : ' + str(response.status_code) + ' ' + response.reason + '"}')
+        else:        
+            #print(response.headers)    
+            try:
+                response = response.json()
+            except:
+                response = "{}"  
+        return response
+
+def pyuipathcloudapi(args):
+    message = ''
+    if args.tenantname == '':
+        itenantname = os.environ.get("UIPATH_TENANTNAME")
+    else:
+        itenantname = args.tenantname
+    if itenantname == None:
+        return {"Error": "tenant name missing! define at least UIPATH_TENANTNAME environment variable"}
+    if args.unit == '':
+        iunit = os.environ.get("UIPATH_UNIT")
+    else:
+        iunit = args.unit    
+    if iunit == None:
+        return  {"Error": "Error : unit missing! define at least UIPATH_UNIT environment variable"}     
+    if args.api == '' and args.jsonfile == '':
+        api="/api/Status/Get"
+    else:
+        api=args.api    
+    if args.url == '':
+        iurl = URL
+    else:
+        iurl = args.url        
+    method = args.method     
+    if "POST" in method and args.jsonfile == "":
+        return {"Error": "Json file required with method POST!"}
+    json = args.jsonfile  
+    if args.uipathfile == '':
+        uipathfile = os.environ.get("UIPATH_FILE")
+    else:
+        uipathfile = args.uipathfile
+    #generate a token first and use this token to call the api.
+    if args.generatetoken == True:
+        if uipathfile == None:
+            return {"Error": "uipath file missing! define at least UIPATH_FILE environment variable"}
+        message= uipathcloudApi.generatetoken(tenantname=itenantname, json=uipathfile)
+        itoken = message['access_token']
+    else:
+        itoken = None
+    message= uipathcloudApi.runuipathcloudApi(api=api, method=method, url=iurl, token=itoken, json=json, unit=iunit, tenant=itenantname) 
+    return message
+
+
+if __name__== "__main__":
+    helpmethod = f"should contain one of the method to use : {str(ALLOWED_METHODS)}"
+    parser = argparse.ArgumentParser(description="pyuipathcloudapi is a python3 program that call uipath cloud apis in command line or imported as a module")
+    parser.add_argument('-V', '--version', help='Display the version of pyuipathcloudapi', action='version', version=pyuipathcloudApiVersion())
+    parser.add_argument('-U', '--unit', help='uipathcloud organization unit id', default='', required=False)    
+    parser.add_argument('-T', '--tenantname', help='uipathcloud tenant name', default='', required=False)            
+    parser.add_argument('-F', '--uipathfile', help='uipathcloud file needed to generate token', default='', required=False)    
+    parser.add_argument('-u', '--url', help='uipathcloud url', default='', required=False)    
+    parser.add_argument('-a', '--api', help='uipathcloud api should start by a slash', default='', required=False)    
+    parser.add_argument('-m', '--method', help = helpmethod, default="GET", required=False)   
+    parser.add_argument('-J', '--jsonfile', help='json file needed for POST method', default='', required=False)
+    parser.add_argument('-g', '--generatetoken', help='generate token first before calling API', action="store_true" , required=False)    
+    args = parser.parse_args()
+    message = pyuipathcloudapi(args)
+    print(message)
